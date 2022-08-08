7770d3edf4303166f55baad3f31ba2aa87e6dccb
Igor_Bit <akinboyboy@gmail.com>
2022-08-08 11:38:52 +0300

    cm >> см

--- 04_my_family.py
+++ 04_my_family.py
@@ -21,5 +21,5 @@ my_family = []
 my_family = [['папа', 180], ['mama', 170], ['сын', 190]]
 farther = my_family [0][1]
 fag = my_family[0][1] + my_family[1][1] + my_family[2][1]
-print('Рост папы =', farther, 'cm')
-print('Рост всей семьи =', fag, 'cm')
+print('Рост папы =', farther, 'см')
+print('Рост всей семьи =', fag, 'см')
